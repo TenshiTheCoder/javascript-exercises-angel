fea82d2684a42fcc170a6f0f435f2e9a7488e087
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2025-02-10 18:30:10 -0500

    Completed Exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let firstString = "";
+  for(let i = 0; i < num; i++){
+    firstString += string;
+  }
 
+  if(num < 0) {
+    return "ERROR";
+  }
+
+  return firstString;
 };
 
+console.log(repeatString("hey", 3))
+console.log(repeatString("hello", 10))
+console.log(repeatString("hi", 1))
+console.log(repeatString("bye", 0))
+console.log(repeatString("goodbye", -1)) 
+console.log("", Math.random() * 1000)
+console.log(repeatString('', 10))
 // Do not edit below this line
 module.exports = repeatString;
