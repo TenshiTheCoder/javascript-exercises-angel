e67990d62f6e9685251ff3978041350686b98982
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2025-02-13 13:20:34 -0500

    Completed exercises 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,33 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+while (year > 0) {
+  if (year % 4 === 0) {
+    year % 100;
+    if (year % 100 !== 0) {
+      return true;
+    } else if (year % 100 === 0) {
+      year % 400;
+      if (year % 400 !== 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+}
+return;
 };
 
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9 / 5 ) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
+
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
